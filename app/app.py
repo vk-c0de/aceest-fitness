85b84da1064e1e7533f9830c83a68a1f4b4e2a61
vk-c0de <mail.pvk25@gmail.com>
2026-04-03 23:55:28 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from app.routes import bp
 
+
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(bp)
     return app
 
+
 if __name__ == "__main__":
     app = create_app()
     app.run(host="0.0.0.0", port=5000)
-
