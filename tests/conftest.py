a9ca95978aea930ed9115599261365761019b7c0
vk-c0de <mail.pvk25@gmail.com>
2026-04-03 23:57:19 +0530

    Add pytest fixture for test client

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 import pytest
 from app.app import create_app
 
+
 @pytest.fixture
 def client():
     app = create_app()
     app.config.update({"TESTING": True})
     with app.test_client() as client:
         yield client
-
